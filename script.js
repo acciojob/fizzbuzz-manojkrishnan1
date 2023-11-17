773704f1812ec5720532a464f0835bc93470cbb3
manojkrishnan1 <127621202+manojkrishnan1@users.noreply.github.com>
2023-11-17 19:32:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+let result = '';
+for(let i=1;i<=20;i++){
+	if(i%3==0 && i%5==0){
+		result+='FizzBuzz\n';
+	}
+	else if(i%3==0){
+		result+='Fizz\n';
+	}
+	else if(i%5==0){
+		result+='Buzz\n';
+	}
+}
+console.log(result);
